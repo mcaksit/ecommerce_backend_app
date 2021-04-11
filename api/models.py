0f0586874d0b72142dbc06a9580311ec38b9f4e8
mcaksit <mehmetaksit@sabanciuniv.edu>
2021-04-11 20:45:04 +0300

    Customer Model and Table Added

--- api/models.py
+++ api/models.py
@@ -3,3 +3,12 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
