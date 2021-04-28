9fd17ae58bfc88774e461c7a21e748360c235fda
mcaksit <mehmetaksit@sabanciuniv.edu>
2021-04-28 21:35:53 +0300

    Added Password to Customer Model

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
     date_created = models.DateTimeField(default=timezone.now)
