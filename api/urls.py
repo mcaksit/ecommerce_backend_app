47cd71362dfbf800801420d2aa37190d116d67bc
mcaksit <mehmetaksit@sabanciuniv.edu>
2021-05-04 00:01:25 +0300

    CORS Policy Issue 4

--- api/urls.py
+++ api/urls.py
@@ -5,16 +5,16 @@ urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
     #User api urls
     path('user-list/', views.UserList, name='user-list'),
-    path('user-detail/<str:pk>/', views.UserDetail, name='user-detail'),
+    path('user-detail/<int:pk>/', views.UserDetail, name='user-detail'),
     path('user-create/', views.UserCreate, name='user-create'),
-    path('user-update/<str:pk>/', views.UserUpdate, name='user-update'),
-    path('user-delete/<str:pk>/', views.UserDelete, name='user-delete'),
+    path('user-update/<int:pk>/', views.UserUpdate, name='user-update'),
+    path('user-delete/<int:pk>/', views.UserDelete, name='user-delete'),
     #Product api urls
     path('search/<str:param>/',views.ProductSearch, name='product-search'),
     path('product-list/', views.ProductList, name='product-list'),
-    path('product-detail/<str:pk>/', views.ProductDetail, name='product-detail'),
+    path('product-detail/<int:pk>/', views.ProductDetail, name='product-detail'),
     path('product-create/', views.ProductCreate, name='product-create'),
-    path('product-update/<str:pk>/', views.ProductUpdate, name='product-update'),
-    path('product-delete/<str:pk>/', views.ProductDelete, name='product-delete'),
+    path('product-update/<int:pk>/', views.ProductUpdate, name='product-update'),
+    path('product-delete/<int:pk>/', views.ProductDelete, name='product-delete'),
 
 ]
