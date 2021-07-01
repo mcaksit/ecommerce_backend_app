7475467819fb1a1769b2ffa4429dbb6414059180
mcaksit <mehmetaksit@sabanciuniv.edu>
2021-07-01 15:28:31 +0300

    V1 Final Commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,13 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4cgmo21%n^w3ayl14^_@ntcf=sc07s^fw55(5_*k+4i^vb3(w%'
 
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
 
 ALLOWED_HOSTS = ['*']
 
